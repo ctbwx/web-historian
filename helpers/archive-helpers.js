317cf615a91be251cbb0ce0b83dffe7341c3367b
Hack Reactor Student <student@hackreactor.com>
2017-09-09 16:54:15 -0500

    Progress with Promises
    Passing 7 out of 9 tests
    
    Needing to refactor code to work with both promises and the results tests are expecting
    
    Currently implementing request-handle file

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,9 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var Promise = require('bluebird')
+var readFileAsync = Promise.promisify(fs.readFile);
+var writeFileAsync = Promise.promisify(fs.writeFile);
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -26,16 +29,59 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  return readFileAsync(this.paths.list)
+    .then(body => callback(body.toString().split('\n')))
+    .catch((error) => console.log('encountered error with readFile', error));
 };
 
 exports.isUrlInList = function(url, callback) {
+
+  return readFileAsync(this.paths.list)
+    .then(body => {
+      let bodyArray = body.toString().split('\n');
+      let isInPromise =  new Promise((resolve, reject) => {
+        if (bodyArray.indexOf(url) !== 1) {
+          resolve(true);
+        } else {
+          reject(false);
+        }
+
+      });
+      return isInPromise
+        .then((result) => callback(true))
+        .catch((error) => callback(false))
+
+    });
+
+
 };
 
 exports.addUrlToList = function(url, callback) {
+  let urlList = [];
+  this.readListOfUrls((readList) => {urlList = readList})
+    .then( list => {
+      // console.log('URL List', urlList);
+      return callback(urlList.push(url));
+    })
+    .catch((error) => {console.log('Error addURL')})
 };
 
 exports.isUrlArchived = function(url, callback) {
+  let desiredSite = this.paths.archivedSites+'/'+url+'';
+  return readFileAsync(desiredSite)
+    .then( body => {
+      callback(true);
+      return true;
+    })
+    .catch( error => {
+      callback(false);
+      return false;
+    })
 };
 
 exports.downloadUrls = function(urls) {
+  for (var url of urls){
+    let desiredPath = this.paths.archivedSites+'/'+url;
+    writeFileAsync(desiredPath, 'Hello');
+  }
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,33 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var Promise = require('bluebird');
+var readFileAsync = Promise.promisify(fs.readFile);
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === 'GET' && req.url === '/') {
+    console.log(process.cwd())
+    readFileAsync('./web/public/index.html')
+      .then(body => res.end(body))
+      .catch(err => console.log(err))
+  } else if (req.method === 'GET') {
+      console.log('Request URL',req.url);
+      let bool;
+      console.log(archive.isUrlArchived(req.url, (value) =>{bool = value}).then(value => value).catch(error => error));
+      console.log('boolean',bool);
+      res.writeHead(404);
+      res.end('Page not found, sorry mate.');
+  }
+  // else {
+  //   res.writeHead(404);
+  //   res.end('Page not found, sorry mate.');
+  // }
+
+  //
+  // else if(req.method === 'GET' && archive.isUrlArchived(req.url, () => {}).then(val => val)) {
+  //   readFileAsync(archive.paths.archivedSites+req.url)
+  //     .then(body => res.end(body))
+  // }
+
 };
